a68c359b66d76d08a36441eda660f7617623b691
Bob Sullivan <besullivan@essvote.com>
2018-08-29 10:40:13 -0500

    FizzBuzz
     * FizzBuzz.cob
       -- Fixed newlines and tabs
       -- Issues: Complex if and leading zeros on integers.

--- FizzBuzz.cob
+++ FizzBuzz.cob
@@ -10,6 +10,7 @@
        WORKING-STORAGE SECTION.
        01 W-I PIC 999 VALUE 1.
        01 W-F PIC 999 VALUE 0.
+       01 W-TAB PIC A VALUE X"09".
        PROCEDURE DIVISION.
        MAIN-PROCEDURE.
            PERFORM WHILE-LOOP.
@@ -19,22 +20,22 @@
              COMPUTE W-F = 0
 
              IF (FUNCTION MOD(W-I, 15) = 0) AND (W-F = 0)
-               DISPLAY 'FizzBuzz'
+               DISPLAY 'FizzBuzz' W-TAB WITH NO ADVANCING
                COMPUTE W-F = 1
              END-IF
 
              IF (FUNCTION MOD(W-I, 5) = 0) AND (W-F = 0)
-               DISPLAY 'Fizz'
+               DISPLAY 'Fizz' W-TAB WITH NO ADVANCING
                COMPUTE W-F = 1
              END-IF
 
              IF (FUNCTION MOD(W-I, 3) = 0) AND (W-F = 0)
-               DISPLAY 'Buzz'
+               DISPLAY 'Buzz' W-TAB WITH NO ADVANCING
                COMPUTE W-F = 1
              END-IF
 
              IF (W-F = 0)
-               DISPLAY W-I
+               DISPLAY W-I W-TAB WITH NO ADVANCING
              END-IF
 
              COMPUTE W-I = W-I + 1
